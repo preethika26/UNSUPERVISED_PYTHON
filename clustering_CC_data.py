ed9846d6cf101068877a588dbd77d2f51c4f0d5c
preethika26 <54073094+preethika26@users.noreply.github.com>
2019-12-16 14:32:41 +0530

    Add files via upload

--- clustering_CC_data.py
+++ clustering_CC_data.py
@@ -22,6 +22,38 @@ credit.median()
 credit['MINIMUM_PAYMENTS']=credit['MINIMUM_PAYMENTS'].fillna((credit['MINIMUM_PAYMENTS'].mean()))
 credit['CREDIT_LIMIT']=credit['CREDIT_LIMIT'].fillna((credit['CREDIT_LIMIT'].median()))
 
+##DIMENSIONALITY REDUCTION USING PRONICIPLE COMPONENT ANALYSIS
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+
+
+x=pd.DataFrame(credit)
+x=sc.fit_transform(x)
+x=pd.DataFrame(x)
+
+from sklearn.decomposition import PCA
+pca=PCA()
+x=pca.fit_transform(x)
+
+res=pca.explained_variance_ratio_*100  ##to see in % multiply by 100
+a=np.cumsum(pca.explained_variance_ratio_*100) ##cumulative sum
+
+##SCREE PLOT
+
+scores=pd.Series(pca.components_[0])
+scores.abs().sort_values(ascending=False)
+
+var=pca.components_[0]
+plt.bar(x=range(1,len(var)+1),height=res)
+plt.show()
+
+##remove the variables according to PCA
+crt_data=pd.DataFrame(credit)
+c=crt_data.drop(['INSTALLMENTS_PURCHASES','CASH_ADVANCE_FREQUENCY','PAYMENTS','PURCHASES_INSTALLMENTS_FREQUENCY','PRC_FULL_PAYMENT'],axis=1)
+
 ##fit elbow curve
 from sklearn.cluster import KMeans
 
@@ -29,46 +61,33 @@ wcss=[]
 
 for i in range(1,11):
     k=KMeans(i,init='k-means++',random_state=54)
-    k.fit(credit)
+    k.fit(c)
     wcss.append(k.inertia_)
     
 import matplotlib.pyplot as plt
-
 plt.plot(range(1,11),wcss)  
 
-##predit the cluster
-mat = credit.values
+##pipelines combine scaler and kmeans
+from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+
+scaler=StandardScaler()
 km = KMeans(n_clusters=7)
-km.fit(mat)
 
-# Get cluster assignment labels
-labels = km.labels_
+from sklearn.pipeline import make_pipeline
+pipeline=make_pipeline(scaler,km)
 
-# Format results as a DataFrame
-results = pd.DataFrame([credit.index,labels]).T
+##predict the cluster
+pred=pipeline.fit_predict(c)
 
-##remove the first column since it is s.no
-cluster=results[1] 
+##convert the predicted to dataframe
+pred=pd.DataFrame(pred)
 
-##give a name to the cluster
-cluster=pd.DataFrame(cluster)
-cluster.columns=["cluster"]
+##name the column as cluster
+pred.columns=["cluster"]
 
 ##combine cluster and data into one data frame
-data=pd.concat([credit, cluster], axis=1)
-
-##save the data
-data.to_csv('data.csv', index=False)
-
-
-
-
-
-
-
-
-
+data=pd.concat([c, pred], axis=1)
 
 
 
